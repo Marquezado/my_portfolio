17a7941eb9ca65a506a7bc8de4c8f746aa78b78f
David <marquezadod4vid@gmail.com>
2025-07-11 15:28:20 -0500

    add deploy configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import path from 'path';
 
-// https://astro.build/config
 export default defineConfig({
-    site: 'https://Marquezado.github.io',
-    base: 'my_portfolio'
+  site: 'https://Marquezado.github.io',
+  base: '/my_portfolio/',
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+        '@cv': path.resolve('./cv.json')
+      }
+    }
+  }
 });
